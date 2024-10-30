1345e24dca5065f75e0edeff19635e01da6869b2
jancis <janciskauls@gmail.com>
2024-10-30 15:58:29 +0200

    added basic code for creating a 16x16 grid

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+const container = document.querySelector("#container");
+
+for(var i=0; i<16; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+
+    for(var j=0; j<16; j++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        row.appendChild(square);
+    }
+}
